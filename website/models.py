0bb7aef082191841c00ab72bba625a74a0a122f1
unknown <kauanyvieira697@gmail.com>
2026-04-23 09:08:50 -0300

    aula 2304

--- website/models.py
+++ website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Usuario (models.Model):
+    nome = models.CharField()
+    cpf = models.CharField()
+    gmail = models.CharField()
+    senha = models.CharField()
+    
+class Admin (Usuario):
+    None
+    
+class Instrutor (Usuario):
+    curso = models.CharField()
+     
+class Aluno(Usuario):
+    None
+
+class Curso (models.Model):
+    nome = models.CharField()
+    data_hora = models.DateTimeField()
+    
+    
+    
